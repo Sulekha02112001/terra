7223c92510a392aded4495a95c6bca99d74f54fe
Sulekha02112001 <64317049+Sulekha02112001@users.noreply.github.com>
2020-06-14 13:19:48 +0530

    Create test.tf

---
+++ test.tf
@@ -0,0 +1,19 @@
+  provider "aws" {
+  region     = "ap-south-1"
+  profile    = "sulekhakey"
+}
+
+
+
+
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+}
+
+  module "key_pair" {
+
+   source = "terraform-aws-modules/key-pair/aws"
+    key_name   = "key123"
+    public_key = tls_private_key.key.public_key_openssh
+}
